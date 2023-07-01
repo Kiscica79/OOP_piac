a24ff789037f67e44804ef03d3a496c9182d5948
Kiscica79 <oroszildiko79@gmail.hu>
2023-07-01 16:09:57 +0200

    Work in progress

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -18,10 +17,12 @@ public class Main {
                 case "édes" -> iz = Iz.EDES;
             }
              */
-            Termek actualPiac = new Termek(lineAsArray[0], lineAsArray[1],
-                    Integer.parseInt(lineAsArray[2]), Boolean.parseBoolean(lineAsArray[3]),
-                    "sós".equals(lineAsArray[4]) ? Iz.SOS : Iz.EDES);
-                    // iz simán a fenti equals helyett
+            Termek actualPiac = new Termek(lineAsArray[0],
+                    lineAsArray[1],
+                    Integer.parseInt(lineAsArray[2]),
+                    Boolean.parseBoolean(lineAsArray[3]),
+                    lineAsArray[4].equals("sós") ? Iz.SOS : Iz.EDES;
+            // iz simán a fenti equals helyett
 
             piac.add(actualPiac);
 
@@ -31,29 +32,63 @@ public class Main {
         }
 
         // TODO hány db hatósági áras termék van?
+        int count = 0;
         for (var actualTermek : piac) {
             if (actualTermek.isHatosagiAr()) {
-                System.out.println(actualTermek);
+                count++;
             }
         }
+        System.out.println(count);
+
         // TODO Mekkora a sós és édes sütemények aránya?
+        int countSos = 0;
         for (var actualTermek : piac) {
-            if (actualTermek.getKategoria().equals("sós")) {
-                System.out.println(actualTermek);
+            if (actualTermek.kategoria().equals(Iz.SOS)) {
+                countSos++;
             }
         }
-        /*
+        System.out.println(countSos);
+
+
         // TODO Mi a legdrágább, és a legolcsóbb?
-        for (var actualTermek : piac) {
-            int legdragabb = actualTermek[0];
-            if (actualTermek.getAr() > legdragabb) {
-                    legdragabb = actualTermek.getAr();
+        Termek legdragabb = piac.get(0);
+        Termek legolcsobb = piac.get(0);
+        for (Termek actualTermek : piac) {
+            if (actualTermek.ar() > legdragabb.ar()) {
+                legdragabb = actualTermek;
             }
-            System.out.println(actualTermek);
+            if (actualTermek.ar() < legolcsobb.ar()) {
+                legolcsobb = actualTermek;
+            }
+            System.out.println("Ez a termék a legdrágább: " + legdragabb.nev() + " aminek az ára: " +
+                    legdragabb.ar());
+            System.out.println("Ez a termék a legolcsóbb:" + legolcsobb.nev() + "aminek az ára: " +
+                    legolcsobb.nev());
+
+
+            // TODO Melyik termék kapható a legtöbb helyen?
+            //  Van olyan hely, ahol több termék is elérhető?
+
+            Map<String, Integer> countPlaces = new HashMap<>();
+            for (var actual : piac) {
+                List<String> placesOfPurchase = actual.placesOfPurchase();
+                for (String place : placesOfPurchase) {
+                    countPlaces.put(place, countPlaces.getOrDefault(place, 0) + 1);
+                }
+            }
+
+            int maxItem = 0;
+            String mostPlace = "";
+            for (Map.Entry<String, Integer> entry : countPlaces.entrySet()) {
+                if (entry.getValue() > maxItem) {
+                    maxItem = entry.getValue();
+                    mostPlace = entry.getKey();
+
+                }
+            }
+            System.out.println("Legtöbb helyen kapható termék száma: " + maxItem);
+            System.out.println("Az elérhető legtöbb hely: " + mostPlace);
         }
-
-         */
-
-
     }
 }
+
--- src/Termek.java
+++ src/Termek.java
@@ -1,58 +1,9 @@
+import java.util.List;
+
 // úgy is lehetne, hogy class helyett record, és akkor a Termék után vannak felsorolva a fieldek
-public class Termek {
-    private String nev;
-    private String egyseg;
-    private int ar;
-    private boolean hatosagiAr;
-    private Iz kategoria;
+public record Termek(String nev, String egyseg, int ar, boolean hatosagiar, Iz kategoria,
+                     List<String> placesOfPurchase) {
 
-    public Termek(String nev, String egyseg, int ar, boolean hatosagiAr, Iz kategoria) {
-        this.nev = nev;
-        this.egyseg = egyseg;
-        this.ar = ar;
-        this.hatosagiAr = hatosagiAr;
-        this.kategoria = kategoria;
-    }
-
-    public String getNev() {
-        return nev;
-    }
-
-    public void setNev(String nev) {
-        this.nev = nev;
-    }
-
-    public String getEgyseg() {
-        return egyseg;
-    }
-
-    public void setEgyseg(String egyseg) {
-        this.egyseg = egyseg;
-    }
-
-    public int getAr() {
-        return ar;
-    }
-
-    public void setAr(int ar) {
-        this.ar = ar;
-    }
-
-    public boolean isHatosagiAr() {
-        return hatosagiAr;
-    }
-
-    public void setHatosagiAr(boolean hatosagiAr) {
-        this.hatosagiAr = hatosagiAr;
-    }
-
-    public Iz getKategoria() {
-        return kategoria;
-    }
-
-    public void setKategoria(Iz kategoria) {
-        this.kategoria = kategoria;
-    }
 
     @Override
     public String toString() {
@@ -60,8 +11,14 @@ public class Termek {
                 "nev='" + nev + '\'' +
                 ", egyseg='" + egyseg + '\'' +
                 ", ar=" + ar +
-                ", hatosagiAr=" + hatosagiAr +
+                ", hatosagiar=" + hatosagiar +
                 ", kategoria=" + kategoria +
+                ", placesOfPurchase=" + placesOfPurchase +
                 '}';
     }
+
+    public boolean isHatosagiAr() {
+        return true;
+    }
+
 }
